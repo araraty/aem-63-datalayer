61c996d3936182dd04aebdacb15a48e6e9a7ed59
Yuval Ararat <ararat@adobe.com>
2017-11-13 15:22:31 +1300

    Adding tests

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # aem-63-datalayer
-AEM 6.3 Datalayer implementation
+AEM 6.3 Datalayer sample implementation with matching AnalyticsPageNameProvider implementation to support the AEM import and content insight support when using a custom pageName.
+##Data Layer
+A sling Model implementation supporting page and user details to show the ability to use resource and request in relation to the page information.
+Works with JSONP implementation javax.json.JsonObject to align with the removal of JSONObject from AEM.
+##AnalyticsPageNameProvider implementation
+This changes the page name mechanism.
+
+##Building the project
+Will build and deploy using maven.
+Will require implementation to a demo page which i have not included.
+Supports:
+    mvn clean install
+and
+    mvn clean install -PautoInstallPackage
+
+Or to deploy it to a publish instance
+
+    mvn clean install -PautoInstallPackagePublish
+
+And Author
+
+    mvn clean install -PautoInstallBundle
+
+## Testing
+WIP at this stage. will be included soon.
+
---
+++ core/src/test/java/com/adobe/people/ararat/datalayer/core/models/DataLayerTest.java
@@ -0,0 +1,50 @@
+package com.adobe.people.ararat.datalayer.core.models;
+
+
+import com.adobe.acs.commons.models.injectors.annotation.impl.AemObjectAnnotationProcessorFactory;
+import com.adobe.acs.commons.models.injectors.impl.AemObjectInjector;
+import io.wcm.testing.mock.aem.junit.AemContext;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.testing.mock.sling.ResourceResolverType;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DataLayerTest{
+
+
+    @Rule
+    public final AemContext context = new AemContext(ResourceResolverType.JCR_MOCK);
+
+    @Mock
+    private SlingHttpServletRequest request;
+
+
+    private static final String PAGE_PATH = "/content/website/en/homepage";
+
+    private DataLayer mymodel;
+    private AemObjectInjector aemObjectInjector;
+    private AemObjectAnnotationProcessorFactory factory;
+
+    @Before
+    public final void setUp() throws Exception {
+        context.addModelsForClasses(DataLayer.class);
+        context.load().json("/page.json", PAGE_PATH);
+        context.currentPage(PAGE_PATH);
+        aemObjectInjector = new AemObjectInjector();
+        context.registerService(AemObjectInjector.class, aemObjectInjector);
+        request = context.request();
+        mymodel = request.adaptTo(DataLayer.class);
+    }
+
+    @Test
+    public void isEmptyStringTest() {
+        Assert.assertNotNull(mymodel);
+        Assert.assertNotSame("",mymodel.getDatalayer());
+    }
+}
